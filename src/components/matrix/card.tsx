fa41295bc61a054782c00ad90f3bf45a08dbcc07
crscnc <dc3@mailbox.org>
2025-03-26 18:30:50 +0000

    better

--- src/components/matrix/card.tsx
+++ src/components/matrix/card.tsx
@@ -1,26 +1,27 @@
 import React from "react";
-import { BeakerIcon, ArchiveBoxIcon } from "@heroicons/react/24/solid";
-
+import { BeakerIcon, ArchiveBoxIcon, SunIcon } from "@heroicons/react/24/solid";
 import styles from "./card.module.css";
 
-export default function Card({ areaDesc, tag, rgUnitListCount }) {
-  const boxCount = Math.min(rgUnitListCount, 16);
+export default function Card({ areaDesc, tag, rgUnitListCount, areaSubType }) {
+  const boxCount = Math.min(rgUnitListCount, 25);
+
   return (
     <div className={styles.card}>
       <section>
         <div className={styles.cardHeader}>
           <div className={styles.leftSide}>
-            {tag}
+            <SunIcon className="size-5 text-blue-500" />
+
             <span className={styles.cardTitle}>{areaDesc}</span>
           </div>
 
           <div className={styles.rightSide}>
-            <BeakerIcon />
-            <ArchiveBoxIcon />
-            <BeakerIcon />
-            <ArchiveBoxIcon />
-            <BeakerIcon />
-            <ArchiveBoxIcon />
+            <BeakerIcon className="size-4 text-blue-500" />
+            <ArchiveBoxIcon className="size-4 text-blue-500" />
+            <BeakerIcon className="size-4 text-blue-500" />
+            <ArchiveBoxIcon className="size-4 text-blue-500" />
+            <BeakerIcon className="size-4 text-blue-500" />
+            <ArchiveBoxIcon className="size-4 text-blue-500" />
           </div>
         </div>
       </section>
